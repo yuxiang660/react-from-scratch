961c294f8b2a0332dcaff907e0a03cf9d7406dc6
yuxiang660 <yuxiang660@163.com>
2020-01-29 16:08:21 +0800

    import scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
+import './styles/main.scss';
 
 ReactDOM.render(
   <MuiThemeProvider>
