ed476751d04ba4b72f1fab59cb948b20fda729e5
yuxiang660 <yuxiang660@163.com>
2020-01-31 10:55:19 +0800

    Add redux dev tool

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { reducers } from "./reducers/index";
 import { sagas } from "./sagas/index";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 let middlewares = [];
 const sagaMiddleware = createSagaMiddleware();
 middlewares.push(sagaMiddleware);
 
-let middleware = applyMiddleware(...middlewares);
-const store = createStore(reducers, middleware);
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(...middlewares)
+));
 
 sagaMiddleware.run(sagas);
 
